990f3d99d3bb30939bd34361f974a830932e4592
Bearice Ren <bearice@icybear.net>
2022-04-03 22:15:19 +0800

    remove font.bmp

--- resource/font.py
+++ resource/font.py
@@ -9,7 +9,7 @@ font = ImageFont.truetype("resource/Px437_DOS-V_TWN16.ttf", size=16)
 for i in range(128):
     draw.text((0, i*height), chr(i), fill=1, font=font)
 
-image.save('font.bmp', 'bmp')
+# image.save('resource/font.bmp', 'bmp')
 pixels = list(image.getdata())
 
 print("#File_format=Bin")
