9135a907d81599397582c54932b338b6bd644b64
Bearice Ren <bearice@icybear.net>
2022-04-05 16:16:34 +0800

    vga support 4 direction selectable by input port

--- src/lcd_vga.v
+++ src/lcd_vga.v
@@ -3,50 +3,41 @@ module LcdVga (
         input clk_pix,
         input reset,
 
-        output LCD_DEN,
         output LCD_CLK,
-        output LCD_HSYNC,
-        output LCD_VSYNC,
+        output reg LCD_DEN,
+        output reg LCD_HSYNC,
+        output reg LCD_VSYNC,
         output reg [15:0] LCD_DATA,
         output reg frame_int,
 
+        input [1:0] direction,
         input ram_clk,
         input ram_reset,
         input ram_ce,
         input [15:0] ram_data,
         input [11:0] ram_addr
-
     );
-    localparam H_BackPorch = 16'sd46;
+    localparam H_BackPorch = 16'd46;
     localparam H_Pluse = 16'd1;
-    localparam H_Data = 16'sd800;
-    localparam H_FrontPorch= 16'sd294; //was 210, use 294 to make 60Hz frame rate @clk 36Mhz
+    localparam H_Data = 16'd800;
+    localparam H_FrontPorch= 16'd294; //was 210, use 294 to make 60Hz frame rate @clk 36Mhz
 
-    localparam V_BackPorch = 16'sd23;
+    localparam V_BackPorch = 16'd23;
     localparam V_Pluse = 16'd5;
-    localparam V_Data = 16'sd480;
-    localparam V_FrontPorch= 16'sd23;
+    localparam V_Data = 16'd480;
+    localparam V_FrontPorch= 16'd23;
 
     localparam H_Max = H_Data + H_BackPorch + H_FrontPorch;
     localparam V_Max = V_Data + V_BackPorch + V_FrontPorch;
 
-    localparam FontWidth = 8;
-    localparam FontHeight = 16;
-
-    localparam CharPerLine = H_Data / FontWidth / 2;
-    localparam LinePerPage = V_Data / FontHeight / 2;
-
     localparam pipeline_length = 6;
 
-    reg signed [15:0] p0_x;
-    reg signed [15:0] p0_y;
+    reg [15:0] p0_x;
+    reg [15:0] p0_y;
 
     wire x_en = p0_x >= H_BackPorch && p0_x < H_Max-H_FrontPorch;
     wire y_en = p0_y >= V_BackPorch && p0_y < V_Max-V_FrontPorch;
 
-    assign LCD_HSYNC = p0_x[14:0] > H_Pluse;
-    assign LCD_VSYNC = p0_y[14:0] > V_Pluse;
-    assign LCD_DEN = x_en & y_en;
     assign LCD_CLK = ~clk_pix; //clk_pix is inverted to avoid setup time issue
 
     // phase 0: clock generation
@@ -65,41 +56,70 @@ module LcdVga (
             end else begin
                 p0_x <= p0_x + 1;
             end
+            LCD_HSYNC <= p0_x > H_Pluse;
+            LCD_VSYNC <= p0_y > V_Pluse;
+            LCD_DEN <= x_en & y_en;
             frame_int <= p0_y == V_Data + V_BackPorch;
+
             p00_x <= p0_x-H_BackPorch + pipeline_length; // if we have a N stage pipeline for each pixel, we need to add N to x in advance.
             p00_y <= p0_y-V_BackPorch; // line number doesnt need to mangle because pipeline_length is smaller than V_FrontPorch
         end
     end
 
     // phase 0.5: map cordinates
-    reg signed [15:0] p00_x;
-    reg signed [15:0] p00_y;
+    localparam FontWidth = 8;
+    localparam FontHeight = 16;
+
+    reg [15:0] p00_x;
+    reg [15:0] p00_y;
     always @(posedge clk_pix) begin
-        // flip upside down, reduce resolution by 2x
-        p1_x <= ((H_Data-1)-p00_x)>>1;
-        p1_y <= ((V_Data-1)-p00_y)>>1;
-        p1_en <= p00_x>=0 && p00_y>=0 && p00_x<H_Data && p00_y<V_Data;
+        p1_en <= p00_x<H_Data && p00_y<V_Data;
+        //reduce resolution by 2x, rotate to direction
+        case (direction)
+            0: begin
+                p1_x <= p00_x >> 1; // / 2;
+                p1_y <= p00_y >> 1; // / 2;
+                p1_cpl <= H_Data/FontWidth/2;
+            end
+            1: begin
+                p1_x <= (V_Data/2-1)-(p00_y>>1);
+                p1_y <= p00_x >> 1;
+                p1_cpl <= V_Data/FontWidth/2;
+            end
+            2: begin
+                p1_x <= (H_Data/2-1)-(p00_x>>1);
+                p1_y <= (V_Data/2-1)-(p00_y>>1);
+                p1_cpl <= H_Data/FontWidth/2;
+            end
+            3: begin
+                p1_x <= (p00_y>>1);
+                p1_y <= (H_Data/2-1)-(p00_x>>1);
+                p1_cpl <= V_Data/FontWidth/2;
+            end
+        endcase
     end
 
 
+
     // phase1: map pixel to text address
-    reg signed [15:0] p1_x;
-    reg signed [15:0] p1_y;
+    reg [15:0] p1_x;
+    reg [15:0] p1_y;
+    reg [15:0] p1_cpl; // char per line
     reg p1_en;
     always @(posedge clk_pix) begin
         if (p1_en) begin
             p2_en <= 1;
             p2_x <= p1_x;
             p2_y <= p1_y;
-            p2_addr <= (p1_x / FontWidth) + ((p1_y / FontHeight) * CharPerLine);
+            p2_addr <= (p1_x >> 3) + ((p1_y >> 4) * p1_cpl);
         end else
             p2_en <= 0;
     end
 
     // phase2: read text address, map to pixel address
     reg p2_en;
-    reg signed [15:0] p2_x;
-    reg signed [15:0] p2_y;
+    reg [15:0] p2_x;
+    reg [15:0] p2_y;
     reg [9:0] p2_addr; //Address of the text ram
     wire [15:0] p2_data; //Data from the text ram
     VRam ram (
